680bb0f1c7d2b8a1cec717c56d0e26184554093a
Sarah Gilbert <sarahgilbert064@gmail.com>
2020-12-10 11:49:15 -0800

    pass first test for ScoreCounterConstructor
    Co-authored-by: Constantine Yakubovski <faustlarsen@gmail.com>

---
+++ ScrabbleScorer/Models/ScoreCounter.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+// using System;
+
+namespace ScrabbleScorer.Models
+{
+  public class ScoreCounter
+  {
+    public string LetterInput { get; set; }
+    private static List<ScoreCounter> _instances = new List<ScoreCounter> {};
+
+    public ScoreCounter(string kindOfLetter)
+    {
+      LetterInput = kindOfLetter;
+      _instances.Add(this);
+    }
+
+  }
+}
