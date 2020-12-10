07fc287706315d5f853fa96993537424baea8fb9
Sarah Gilbert <sarahgilbert064@gmail.com>
2020-12-10 11:45:57 -0800

    purposefully fail first test for ScoreCounterConstructor
    Co-authored-by: Constantine Yakubovski <faustlarsen@gmail.com>

---
+++ ScrabbleScorer.Tests/ModelTests/ScoreCounterTests.cs
@@ -0,0 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ScrabbleScorer.Models;
+using System.Collections.Generic;
+// using System;
+
+namespace ScrabbleScorer.Tests
+{
+  [TestClass]
+  public class ScoreCounterTests
+  {
+    [TestMethod]
+    public void ScoreCounterConstructor_CreatesInstanceOfScoreCounter_ScoreCounter()
+    {
+      ScoreCounter newScore = new ScoreCounter("A");
+      Assert.AreEqual(typeof(ScoreCounter), newScore.GetType());
+    }
+
+
+  }
+}
